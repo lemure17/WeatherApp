fd3e6fb8a0577a5db99330972ab3d9af0afec8a2
Leonardo Lemos dos Santos <leolemossantos@hotmail.com>
2026-06-26 20:47:44 -0300

    finalização atividade 4

--- app/src/main/java/com/example/weatherapp/ui/pages/MapPage.kt
+++ app/src/main/java/com/example/weatherapp/ui/pages/MapPage.kt
@@ -1,13 +1,20 @@
 package com.example.weatherapp.ui.pages
 
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.example.weatherapp.viewmodel.MainViewModel
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -24,11 +31,20 @@ fun MapPage(
     val recife = remember { MarkerState(LatLng(-8.05, -34.9)) }
     val caruaru = remember { MarkerState(LatLng(-8.27, -35.98)) }
     val joaopessoa = remember { MarkerState(LatLng(-7.12, -34.84)) }
-
+    val context = LocalContext.current
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED
+        )
+    }
     GoogleMap(
         modifier = modifier.fillMaxSize(), // Usando o modifier recebido
         cameraPositionState = camPosState,
-        onMapClick = {
+        properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true),
+                onMapClick = {
             viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it)
         }
     ) {
